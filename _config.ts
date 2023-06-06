c542a3c9165be8d798bc7e50f2cf22095d023dcd
Giles Dring <giles@dringtech.com>
2023-06-06 08:27:43 +0100

    Adding imagick compression

--- _config.ts
+++ _config.ts
@@ -1,6 +1,7 @@
 import lume from "lume/mod.ts";
-import esbuild from "lume/plugins/esbuild.ts";
 import base_path from "lume/plugins/base_path.ts";
+import esbuild from "lume/plugins/esbuild.ts";
+import imagick from "lume/plugins/imagick.ts";
 import postcss from "lume/plugins/postcss.ts";
 
 const site = lume({
@@ -16,8 +17,9 @@ site
       // minify: true,
     },
   }))
+  .use(imagick())
   .use(base_path())
-  .use(postcss())
-  .copy([".png", ".jpg"]);
+  .use(postcss());
+  // .copy([".png", ".jpg"]);
 
 export default site;
